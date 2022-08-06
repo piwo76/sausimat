7b14d641c751bd0974e0e63f17dbf1b85b881c51
martin <martin.buehrer@gyrotools.com>
2022-08-06 22:22:20 +0200

    args

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Hex Tag to number')
 parser.add_argument('--arduino-serial', type=str, default='/dev/ttyUSB0', help='the arduino serial found in /dev/tty*')
 parser.add_argument('--baud-rate', type=int, default=9600, help='the arduino baud rate')
-parser.add_argument('--logfile', type=int, default='/var/log/sausimat.log', help='the path to the logfile')
+parser.add_argument('--logfile', type=str, default='/var/log/sausimat.log', help='the path to the logfile')
 
 args = parser.parse_args()
 
